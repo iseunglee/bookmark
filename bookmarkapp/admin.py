ada4235cc103cc9bc43b8c6d86531c8973f5024b
iseunglee <iseunglee99@gmail.com>
2024-04-06 14:49:47 +0900

    4월6일 복습

--- bookmarkapp/admin.py
+++ bookmarkapp/admin.py
@@ -5,3 +5,12 @@ from bookmarkapp.models import Bookmark
 @admin.register(Bookmark)
 class BookmarkAdmin(admin.ModelAdmin):
     list_display = ('id', 'title', 'url')
+
+'''
+class BookmarkAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'url')
+
+admin.site.register(Bookmark, BookmrkAdmin)
+
+데코레이터를 사용하지 않고 다음과 같이 admin 사이트에 테이블을 반영할 수 있다.
+'''
--- bookmarkapp/urls.py
+++ bookmarkapp/urls.py
@@ -8,3 +8,16 @@ urlpatterns = [
 
     path("<int:pk>/", views.BookmarkDV.as_view(), name='detail')
 ]
+
+'''
+클래스형 뷰가 간단한 경우는 views.py에 코딩할 필요없이 URLconf에서 뷰 및 뷰 처리에 필요한 파라미터를 모두 지정할 수 있다.
+하지만 향후 확장성이나 임포트 관계를 단순하게 유지하기 위해 views.py에 작성하는 것이 좋다
+
+즉, 아래와 같이 코딩할 수 있다.
+urlpatterns = [
+    path('', ListView.as_view(model=Bookmark), name='index')
+    path('<int:pk>/', DetailView.as_view(model=Bookmark), name='detail')
+]
+
+다음과 같이 코드를 작성하면 views.py에 코드를 작성할 필요없이 urls.py에서 처리가 가능하다.
+'''
